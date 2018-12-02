22756c060e5567a88459167c5d1fbc181ee6c540
Taisuke Yamada <tai@rakugaki.org>
2018-12-02 11:46:50 +0900

    Added more descriptions in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,39 @@
+# -*- coding: utf-8-unix -*-
+#
+# for user:
+# python setup.py install
+#
+# for maintainer:
+# python setup.py sdist upload -r https://test.pypi.org/legacy/
+# python setup.py sdist upload
+#
+
 from setuptools import setup
 
+# use README.md for long_description
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='morfeus',
-      version='0.2',
+      version='0.3',
       description='Library to control moRFeus mixer/signal generator',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='http://github.com/tai/morfeus',
       author='Taisuke Yamada',
       author_email='tai.nospam@nospam.rakugaki.org',
       license='MIT',
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Topic :: Communications :: Ham Radio",
+          "Topic :: System :: Hardware :: Hardware Drivers",
+          "Intended Audience :: Science/Research",
+          "Intended Audience :: Telecommunications Industry"
+      ],
       packages=['morfeus'],
       scripts = ['bin/morfeus'],
       install_requires=['hidapi', 'argparse'],
